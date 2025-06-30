1504556502cbb1d1804dc764f7470b0fc4e61469
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2025-06-30 23:54:15 +0530

    Add files via upload

---
+++ star_pattern14.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class star_pattern14 {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+
+    int sp=0,st=n;
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=sp;j++)
+        {
+            System.out.print("\t");
+        }
+        for(int j=1;j<=st;j++)
+        {
+            System.out.print("*\t");
+        }
+
+       
+        if(i<=n/2)
+        {
+            st-=2;
+            sp++;
+            
+        }
+        else{
+            
+            st+=2;
+            sp--;
+        }
+        System.out.println("");
+    }
+    }
+}
