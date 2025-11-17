958fd0f6a0888909abbebbb891379a75f1db1a22
Rohan Patil <160716282+Rohanmpatil16@users.noreply.github.com>
2025-11-17 23:44:49 +0530

    program of reverse stars

---
+++ reverse_stars.java
@@ -0,0 +1,13 @@
+ class reverse_stars{
+    public static void main(String args[])
+    {
+        int n = 5; // Number of rows
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
